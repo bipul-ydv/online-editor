32f9e29751033156bb9c327295b29f0543535f47
Parul Khurana <parul.khurana@jellyfishtechnologies.com>
2017-05-11 16:33:40 +0530

    Spring Security Configured

--- grails-app/init/online/editor/BootStrap.groovy
+++ grails-app/init/online/editor/BootStrap.groovy
@@ -1,8 +1,15 @@
 package online.editor
+import com.online.editor.auth.*;
 
 class BootStrap {
 
     def init = { servletContext ->
+        User user1 = new User(username:"admin",password: "admin").save(flush:true)
+        User user2 = new User(username: "parul",password: "parul").save(flush:true)
+        Role role1 = new Role(authority:"ROLE_ADMIN").save(flush:true)
+        Role role2 = new Role(authority:"ROLE_USER").save(flush:true)
+        UserRole userRole1 = new UserRole(user: user1,role: role1).save(flush:true)
+        UserRole userRole2 = new UserRole(user: user2,role: role2).save(flush:true)
     }
     def destroy = {
     }
