4eddaf16a7fe96859c07a1deeeaa62e9dc9ff374
Bipul Kumar <bkumar212@csc.com>
2018-02-12 00:44:52 +0530

    username Update

--- grails-app/init/online/editor/BootStrap.groovy
+++ grails-app/init/online/editor/BootStrap.groovy
@@ -6,7 +6,7 @@ class BootStrap {
     def init = { servletContext ->
         if (!Role.findByAuthority("ROLE_SWITCH_USER")) {
             User user1 = new User(username: "admin", password: "Admin@215",email: "bipulydv@gmail.com").save(flush: true, failOnError: true)
-            User user2 = new User(username: "parul", password: "Parul@215",email: "bipulydv@gmail.com").save(flush: true, failOnError: true)
+            User user2 = new User(username: "jessy", password: "Jessy@215",email: "bipulydv@gmail.com").save(flush: true, failOnError: true)
             Role role1 = new Role(authority: "ROLE_SWITCH_USER").save(flush:true,failOnError: true)
             Role role2 = new Role(authority: "ROLE_USER").save(flush: true, failOnError: true)
             UserRole userRole1 = new UserRole(user: user1, role: role1).save(flush: true, failOnError: true)
